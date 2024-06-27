a58864a05b61d414be05f94164bef595d8f5b1b6
Alex <88832486+AlexRetortJS@users.noreply.github.com>
2024-06-27 15:28:05 +0100

    Remove the rest of autoinstall support from publish.js

--- vue-tags-input/publish.js
+++ vue-tags-input/publish.js
@@ -3,7 +3,7 @@ import { createClasses, createTag, createTags } from '../vue-tags-input/create-t
 import TagInput from '../vue-tags-input/tag-input.vue';
 
 // add autoinstall support if the component is attached to the windows object e.g. if added by CDN
-VueTagsInput.install = Vue => Vue.component(VueTagsInput.name, VueTagsInput);
+// VueTagsInput.install = Vue => Vue.component(VueTagsInput.name, VueTagsInput);
 // if (typeof window !== 'undefined' && window.Vue) window.Vue.use(VueTagsInput);
 
 export {
